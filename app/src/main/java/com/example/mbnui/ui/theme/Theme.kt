b894312be0dfb0d7891c717d4de95cfc9d968c73
takeharanaoki <devadeepmind@gmail.com>
2026-02-04 19:38:40 +0900

    refactor: reorder imports in Theme.kt

--- app/src/main/java/com/example/mbnui/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mbnui/ui/theme/Theme.kt
@@ -2,13 +2,16 @@ package com.example.mbnui.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -21,10 +24,6 @@ private val LightColorScheme = lightColorScheme(
     secondary = PurpleGrey40,
     tertiary = Pink40
 )
-
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Shapes
-import androidx.compose.ui.unit.dp
 
 private val ExpressiveShapes = Shapes(
     extraSmall = RoundedCornerShape(8.dp),
