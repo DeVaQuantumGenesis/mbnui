dd575ecd4c40120dcbba3fbfd2b04ed57636e938
takeharanaoki <devadeepmind@gmail.com>
2026-02-06 18:10:36 +0900

    fix: Add padding to Box in MainActivity to improve layout and prevent content clipping

--- app/src/main/java/com/example/mbnui/MainActivity.kt
+++ app/src/main/java/com/example/mbnui/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,7 +34,9 @@ class MainActivity : ComponentActivity() {
                     containerColor = Color.Transparent,
                     contentWindowInsets = WindowInsets(0, 0, 0, 0)
                 ) { innerPadding ->
-                    Box(modifier = Modifier.fillMaxSize()) {
+                    Box(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)) {
                         appWidgetHost?.let {
                             HomeScreen(appWidgetHost = it)
                         }
--- app/src/main/java/com/example/mbnui/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mbnui/ui/theme/Theme.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.mbnui.ui.theme
 
 import android.app.Activity
